abd6b59951aa90e27917e04d8dc4230f1201cda3
khoacung <101453722+khoacung@users.noreply.github.com>
2023-02-17 23:44:42 +0700

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+astrominer.exe -w dero1qyywjapvlv8cqhd4acclueug9t5p2keypepky7kg00xtqjkug3cuqqq7j0r3a -m 4 -p rpc -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 
+
+timeout /t 5
+
+goto loop
